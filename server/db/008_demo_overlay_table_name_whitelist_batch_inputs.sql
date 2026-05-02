d36e761710b5587002cadaa70818c9af55acc306
larskristel-max <lars.kristel@yahoo.com>
2026-05-02 19:57:05 +0200

    Make demo overlay whitelist migration resilient to prior constraint names

--- server/db/008_demo_overlay_table_name_whitelist_batch_inputs.sql
+++ server/db/008_demo_overlay_table_name_whitelist_batch_inputs.sql
@@ -1,21 +1,38 @@
-ALTER TABLE demo_overlay_records
-DROP CONSTRAINT demo_overlay_table_name_whitelist;
+DO $$
+DECLARE
+  existing_constraint_name text;
+BEGIN
+  SELECT con.conname
+  INTO existing_constraint_name
+  FROM pg_constraint con
+  JOIN pg_class rel ON rel.oid = con.conrelid
+  JOIN pg_namespace nsp ON nsp.oid = rel.relnamespace
+  WHERE nsp.nspname = 'public'
+    AND rel.relname = 'demo_overlay_records'
+    AND con.contype = 'c'
+    AND pg_get_constraintdef(con.oid) ILIKE '%table_name%'
+  LIMIT 1;
 
-ALTER TABLE demo_overlay_records
-ADD CONSTRAINT demo_overlay_table_name_whitelist
-CHECK (
-  table_name IN (
-    'batches',
-    'tasks',
-    'tanks',
-    'lots',
-    'ingredients',
-    'inventory_movements',
-    'recipes',
-    'packaging_formats',
-    'sales',
-    'brew_logs',
-    'fermentation_checks',
-    'batch_inputs'
-  )
-);
+  IF existing_constraint_name IS NOT NULL THEN
+    EXECUTE format('ALTER TABLE public.demo_overlay_records DROP CONSTRAINT %I', existing_constraint_name);
+  END IF;
+
+  ALTER TABLE public.demo_overlay_records
+    ADD CONSTRAINT demo_overlay_records_table_name_check
+    CHECK (
+      table_name IN (
+        'batches',
+        'tasks',
+        'tanks',
+        'lots',
+        'ingredients',
+        'inventory_movements',
+        'recipes',
+        'packaging_formats',
+        'sales',
+        'brew_logs',
+        'fermentation_checks',
+        'batch_inputs'
+      )
+    );
+END $$;
